8447bf18a39260a5de9ec271919909bf7ad30a12
atharv <73892169+HIGAP@users.noreply.github.com>
2024-11-05 21:19:42 +0530

    Update login_page.js

--- scripts/login_page.js
+++ scripts/login_page.js
@@ -6,7 +6,7 @@
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/login', {
+        const response = await fetch('https://higap.onrender.com/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
